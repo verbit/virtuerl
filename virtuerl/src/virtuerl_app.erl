629c2dfd2b833f7573b26e79740a9fb2c363d176
Ilya Verbitskiy <ilya@verbit.io>
2023-08-28 19:45:46 +0200

    virtuerl: add entrypoint for simplistic erl startup

--- virtuerl/src/virtuerl_app.erl
+++ virtuerl/src/virtuerl_app.erl
@@ -7,7 +7,7 @@
 
 -behaviour(application).
 
--export([start/2, stop/1]).
+-export([start/2, stop/1, start/0]).
 
 %% app start/stop
 start(_StartType, _StartArgs) ->
@@ -20,6 +20,9 @@ start(_StartType, _StartArgs) ->
 %%    io:format("RESULT: ~p~n", [Res]),
 %%    {ok, _} = Res,
     virtuerl_sup:start_link().
+
+start() ->
+    application:ensure_all_started(virtuerl).
 
 stop(_State) ->
     ok.
