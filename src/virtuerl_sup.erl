8f2add5a422c73990cc18e5edf1bc28aaf1ce86f
Ilya Verbitskiy <ilya@verbit.io>
2024-06-22 16:25:39 +0200

    format virtuerl_sup.erl

--- src/virtuerl_sup.erl
+++ src/virtuerl_sup.erl
@@ -1,84 +1,61 @@
-%%%-------------------------------------------------------------------
-%% @doc virtuerl top level supervisor.
-%% @end
-%%%-------------------------------------------------------------------
-
 -module(virtuerl_sup).
 
 -behaviour(supervisor).
 
 -export([start_link/0]).
-
 -export([init/1]).
 
 -define(SERVER, ?MODULE).
 
+
 start_link() ->
     supervisor:start_link({local, ?SERVER}, ?MODULE, []).
 
-%% sup_flags() = #{strategy => strategy(),         % optional
-%%                 intensity => non_neg_integer(), % optional
-%%                 period => pos_integer()}        % optional
-%% child_spec() = #{id => child_id(),       % mandatory
-%%                  start => mfargs(),      % mandatory
-%%                  restart => restart(),   % optional
-%%                  shutdown => shutdown(), % optional
-%%                  type => worker(),       % optional
-%%                  modules => modules()}   % optional
-init([]) ->
-    SupFlags = #{strategy => one_for_one,
-                 intensity => 300,
-                 period => 5},
-    ChildSpecs = [
-      {
-        virtuerl_pubsub,
-        {virtuerl_pubsub, start_link, []},
-        permanent,
-        infinity,
-        worker,
-        []
-      }, {
-      virtuerl_ipam,
-      {virtuerl_ipam, start_link, []},
-      permanent,
-      infinity,
-      worker,
-      []
-      }, {
-      virtuerl_img,
-      {virtuerl_img, start_link, []},
-      permanent,
-      infinity,
-      worker,
-      []
-      }, {
-      virtuerl_mgt,
-      {virtuerl_mgt, start_link, []},
-      permanent,
-      infinity,
-      worker,
-      []
-    }, {
-      virtuerl_net,
-      {virtuerl_net, start_link, []},
-      permanent,
-      infinity,
-      worker,
-      []
-    }
-      , {
-      virtuerl_api,
-      {virtuerl_api, start_link, []},
-      permanent,
-      infinity,
-      worker,
-      []
-    }
-    ],
-    OptionalChildSpecs = case application:get_env(gh_pat) of
-      undefined -> [];
-      _ -> [{virtuerl_ghac, {virtuerl_ghac, start_link, []}, permanent, infinity, worker, []}]
-    end,
-    {ok, {SupFlags, ChildSpecs ++ OptionalChildSpecs}}.
 
-%% internal functions
+init([]) ->
+    SupFlags = #{
+                 strategy => one_for_one,
+                 intensity => 300,
+                 period => 5
+                },
+    ChildSpecs = [{virtuerl_pubsub,
+                   {virtuerl_pubsub, start_link, []},
+                   permanent,
+                   infinity,
+                   worker,
+                   []},
+                  {virtuerl_ipam,
+                   {virtuerl_ipam, start_link, []},
+                   permanent,
+                   infinity,
+                   worker,
+                   []},
+                  {virtuerl_img,
+                   {virtuerl_img, start_link, []},
+                   permanent,
+                   infinity,
+                   worker,
+                   []},
+                  {virtuerl_mgt,
+                   {virtuerl_mgt, start_link, []},
+                   permanent,
+                   infinity,
+                   worker,
+                   []},
+                  {virtuerl_net,
+                   {virtuerl_net, start_link, []},
+                   permanent,
+                   infinity,
+                   worker,
+                   []},
+                  {virtuerl_api,
+                   {virtuerl_api, start_link, []},
+                   permanent,
+                   infinity,
+                   worker,
+                   []}],
+    OptionalChildSpecs = case application:get_env(gh_pat) of
+                             undefined -> [];
+                             _ -> [{virtuerl_ghac, {virtuerl_ghac, start_link, []}, permanent, infinity, worker, []}]
+                         end,
+    {ok, {SupFlags, ChildSpecs ++ OptionalChildSpecs}}.
